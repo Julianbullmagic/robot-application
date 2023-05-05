5b691a6aa3d3e89cc0944e20160f75e5037115d0
Julian Bull <julianbullmagic@gmail.com>
2023-05-05 11:13:25 +1000

    ffffff

--- public/js/index.js
+++ public/js/index.js
@@ -8,52 +8,52 @@ const { RTCPeerConnection, RTCSessionDescription } = window;
 const peerConnection = new RTCPeerConnection();
 
 
-let forwardbutton=document.getElementById("forward").onmousedown=function() {
+document.getElementById("forward").onmousedown=function() {
   socket.emit("forward", "forward");
   console.log("forward")
 };
-let backwardbutton=document.getElementById("backward").onmousedown=function() {
+document.getElementById("backward").onmousedown=function() {
   socket.emit("backward", "backward");
   console.log("backward")
 };
-let strafeleftbutton=document.getElementById("strafeleft").onmousedown=function() {
+document.getElementById("strafeleft").onmousedown=function() {
   socket.emit("strafe left", "strafe left");
   console.log("strafe left")
 };
-let straferightbutton=document.getElementById("straferight").onmousedown=function() {
+document.getElementById("straferight").onmousedown=function() {
   socket.emit("strafe right", "strafe right");
   console.log("strafe right")
 };
-let rotateleftbutton=document.getElementById("rotateleft").onmousedown=function() {
+document.getElementById("rotateleft").onmousedown=function() {
   socket.emit("rotate left", "rotate left");
   console.log("rotate left")
 };
-let rotaterightbutton=document.getElementById("rotateright").onmousedown=function() {
+document.getElementById("rotateright").onmousedown=function() {
   socket.emit("rotate right", "rotate right");
   console.log("rotate right")
 };
 
-forwardbutton.onmouseup=function() {
+document.getElementById("forward").onmouseup=function() {
   socket.emit("stop forward", "stop forward");
   console.log("stop forward")
 };
-backwardbutton.onmouseup=function() {
+document.getElementById("backward").onmouseup=function() {
   socket.emit("stop backward", "stop backward");
   console.log("stop backward")
 };
-strafeleftbutton.onmouseup=function() {
+document.getElementById("strafeleft").onmouseup=function() {
   socket.emit("stop strafe left", "stop strafe left");
   console.log("stop strafe left")
 };
-straferightbutton.onmouseup=function() {
+document.getElementById("straferight").onmouseup=function() {
   socket.emit("stop strafe right", "stop strafe right");
   console.log("stop strafe right")
 };
-rotateleftbutton.onomouseup=function() {
+document.getElementById("rotateleft").onomouseup=function() {
   socket.emit("stop rotate left", "stop rotate left");
   console.log("stop rotate left")
 };
-rotaterightbutton.onmouseup=function() {
+document.getElementById("rotateright").onmouseup=function() {
   socket.emit("stop rotate right", "stop rotate right");
   console.log("stop rotate right")
 };
